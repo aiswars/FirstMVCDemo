dddc8e1bb7ec68b3047feb60777a76d6c26d1746
aisdhar <75646664+aisdhar@users.noreply.github.com>
2022-02-25 21:43:04 -0500

    Controller cs classfile created--HomController.cs

---
+++ Controllers/HomController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVCDemo.Controllers
+{
+    public class HomController : Controller
+    {
+        // GET: Hom
+        // public ActionResult Index() //as no view for Index()action method in Views Folder
+        public String Index()
+        {
+            return "Hello MVC 5 Application";
+        }
+    }
+}
